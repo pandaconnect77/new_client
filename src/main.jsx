cafdfa88d79533f4808a114dc091b89194ab713b
Qisguys <122888167+Qisguys@users.noreply.github.com>
2025-05-22 23:00:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.render(
+  <>
+    <Analytics />
     <App />
-  </React.StrictMode>,
-)
+  </>,
+  document.getElementById('root')
+);

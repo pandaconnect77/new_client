f58f2dca8b0ec4c741aa95aef4ddc32e849be110
Panda Connect <122888167+pandaconnect77@users.noreply.github.com>
2025-07-24 13:55:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss(),],
+   define: {
+    global: 'window', // <--- Fix here
+  },
 })
